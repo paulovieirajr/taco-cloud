08869c98ad65bd1160eabe2ed6facfca8e368435
Junior Vieira <junior.vieira.1990@gmail.com>
2022-10-13 21:56:20 -0300

    :sparkles: feat: Controller of Design Taco

---
+++ src/main/java/br/dev/paulovieira/tacocloud/controller/DesignTacoController.java
@@ -0,0 +1,81 @@
+package br.dev.paulovieira.tacocloud.controller;
+
+import br.dev.paulovieira.tacocloud.model.Ingredient;
+import br.dev.paulovieira.tacocloud.model.Taco;
+import br.dev.paulovieira.tacocloud.model.TacoOrder;
+import br.dev.paulovieira.tacocloud.model.enums.Type;
+import br.dev.paulovieira.tacocloud.repository.IngredientRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("tacoOrder")
+public class DesignTacoController {
+
+    IngredientRepository ingredientRepository;
+
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = ingredientRepository.findAll();
+        Type[] types = Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+    }
+
+    @ModelAttribute(name = "tacoOrder")
+    public TacoOrder order() {
+        return new TacoOrder();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @GetMapping
+    public String showDesignForm() {
+        return "design";
+    }
+
+    @PostMapping
+    public String processTaco(@Valid Taco taco,
+                              Errors errors,
+                              @ModelAttribute TacoOrder tacoOrder
+    ) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        taco.setCreatedAt(new Date());
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
+    }
+
+    private Iterable<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+}
+
