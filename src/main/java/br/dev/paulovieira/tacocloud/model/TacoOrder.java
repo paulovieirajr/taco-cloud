c2a65fdd119de2444259475ed3e752e03babf6d4
Junior Vieira <junior.vieira.1990@gmail.com>
2022-10-13 21:48:09 -0300

    :sparkles: feat: TacoOrder Model Class

---
+++ src/main/java/br/dev/paulovieira/tacocloud/model/TacoOrder.java
@@ -0,0 +1,168 @@
+package br.dev.paulovieira.tacocloud.model;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "taco_orders")
+public class TacoOrder implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date placedAt;
+    @NotBlank(message="Delivery name is required")
+    private String deliveryName;
+    @NotBlank(message="Street is required")
+    private String deliveryStreet;
+    @NotBlank(message="City is required")
+    private String deliveryCity;
+    @NotBlank(message="State is required")
+    private String deliveryState;
+    @NotBlank(message="Zip code is required")
+    private String deliveryZip;
+    @CreditCardNumber(message="Not a valid credit card number")
+    private String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])(/)([2-9][0-9])$",
+            message="Must be formatted MM/YY")
+    private String ccExpiration;
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
+    private String ccCVV;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Taco> tacos = new ArrayList<>();
+
+    public TacoOrder() {
+    }
+
+    public void addTaco(Taco taco) {
+        this.tacos.add(taco);
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getPlacedAt() {
+        return placedAt;
+    }
+
+    public void setPlacedAt(Date placedAt) {
+        this.placedAt = placedAt;
+    }
+
+    public String getDeliveryName() {
+        return deliveryName;
+    }
+
+    public void setDeliveryName(String deliveryName) {
+        this.deliveryName = deliveryName;
+    }
+
+    public String getDeliveryStreet() {
+        return deliveryStreet;
+    }
+
+    public void setDeliveryStreet(String deliveryStreet) {
+        this.deliveryStreet = deliveryStreet;
+    }
+
+    public String getDeliveryCity() {
+        return deliveryCity;
+    }
+
+    public void setDeliveryCity(String deliveryCity) {
+        this.deliveryCity = deliveryCity;
+    }
+
+    public String getDeliveryState() {
+        return deliveryState;
+    }
+
+    public void setDeliveryState(String deliveryState) {
+        this.deliveryState = deliveryState;
+    }
+
+    public String getDeliveryZip() {
+        return deliveryZip;
+    }
+
+    public void setDeliveryZip(String deliveryZip) {
+        this.deliveryZip = deliveryZip;
+    }
+
+    public String getCcNumber() {
+        return ccNumber;
+    }
+
+    public void setCcNumber(String ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public String getCcExpiration() {
+        return ccExpiration;
+    }
+
+    public void setCcExpiration(String ccExpiration) {
+        this.ccExpiration = ccExpiration;
+    }
+
+    public String getCcCVV() {
+        return ccCVV;
+    }
+
+    public void setCcCVV(String ccCVV) {
+        this.ccCVV = ccCVV;
+    }
+
+    public List<Taco> getTacos() {
+        return tacos;
+    }
+
+    public void setTacos(List<Taco> tacos) {
+        this.tacos = tacos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TacoOrder tacoOrder = (TacoOrder) o;
+        return getId().equals(tacoOrder.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "TacoOrder{" +
+                "id=" + id +
+                ", deliveryName='" + deliveryName + '\'' +
+                ", deliveryStreet='" + deliveryStreet + '\'' +
+                ", deliveryCity='" + deliveryCity + '\'' +
+                ", deliveryState='" + deliveryState + '\'' +
+                ", deliveryZip='" + deliveryZip + '\'' +
+                ", ccNumber='" + ccNumber + '\'' +
+                ", ccExpiration='" + ccExpiration + '\'' +
+                ", ccCVV='" + ccCVV + '\'' +
+                ", tacos=" + tacos +
+                '}';
+    }
+}
