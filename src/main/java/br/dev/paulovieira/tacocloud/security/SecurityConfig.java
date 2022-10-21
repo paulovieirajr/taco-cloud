d2af01b3c08e7a4ec2e92081101c8b9d555f06c6
Junior Vieira <junior.vieira.1990@gmail.com>
2022-10-20 22:41:55 -0300

    :lock: security: Beans for Spring Security

---
+++ src/main/java/br/dev/paulovieira/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package br.dev.paulovieira.tacocloud.security;
+
+import br.dev.paulovieira.tacocloud.repository.*;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.web.*;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            var user = userRepo.findByUsername(username);
+            if (user != null) return user;
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders").access("hasRole('USER')")
+                .antMatchers("/", "/**").access("permitAll()")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
+                .and()
+                .build();
+    }
+}
