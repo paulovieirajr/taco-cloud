83ecb499ad39f6d7468dd7ebda870119a347183e
Junior Vieira <junior.vieira.1990@gmail.com>
2022-10-22 21:09:51 -0300

    :ok_hand: style: Added a new method to find an Order

--- src/main/java/br/dev/paulovieira/tacocloud/repository/TacoOrderRepository.java
+++ src/main/java/br/dev/paulovieira/tacocloud/repository/TacoOrderRepository.java
@@ -1,15 +1,18 @@
 package br.dev.paulovieira.tacocloud.repository;
 
-import br.dev.paulovieira.tacocloud.model.TacoOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
+import br.dev.paulovieira.tacocloud.model.*;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.*;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public interface TacoOrderRepository extends JpaRepository<TacoOrder, Long> {
 
     List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
     List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
             String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
 
